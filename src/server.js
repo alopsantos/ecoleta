ed44c572e87818c41e2d3ceaf572399010b0c785
Anderson Lopes dos Santos <lopscorp@iMac-de-Anderson.local>
2020-06-08 23:40:55 -0300

    configuracoes iniciais nunjuck

--- src/server.js
+++ src/server.js
@@ -1,15 +1,23 @@
 const express = require("express")
 const server = express()
+const nunjucks = require("nunjucks")
 
 server.use(express.static("public"))
 
-server.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html")
+nunjucks.configure("src/views", {
+  express: server,
+  noCache: true
 })
-server.get("/create-point", (req, res) => {
-  res.sendFile(__dirname + "/views/create-point.html")
+
+server.get("/", (req, res) =>{
+  return res.render("index.html")
 })
-server.get("/search-results", (req, res) => {
-  res.sendFile(__dirname + "/views/search-results.html")
+server.get("/create-point", (req, res) =>{
+  return res.render("create-point.html")
 })
+
+server.get("/search-results", (req, res) =>{
+  return res.render("search-results.html")
+})
+
 server.listen(3000)
